db480cdd3505cbbf894ccbb91400e3b3326f85a2
sriraam-gif <sriraam.chandran@gmail.com>
2020-07-04 12:37:57 -0400

    1st draft of program to find root of a number

---
+++ src/com/company/Main.java
@@ -0,0 +1,98 @@
+package com.company;
+import java.util.*;
+public class Main
+{
+
+    public static void main(String[] args)
+    {
+	// write your code here
+
+        Scanner in=new Scanner(System.in);
+        System.out.println("Enter a number: ");
+        double num=in.nextDouble();
+        System.out.println("What power root");
+        int root=in.nextInt();
+        long start=System.currentTimeMillis();
+        double rel=bisec(num, root);
+        long end=System.currentTimeMillis();
+        System.out.println("Bisection: "+ rel);
+        System.out.println("Time : "+ (end-start)+" milliseconds");
+
+
+    }
+
+    public static double bisec(double c, int d)
+    {
+        if(c<0 && d%2==0)
+        {
+            return -1;
+        }
+        else if(d<0)
+        {
+            return -1;
+        }
+        else if(d==0)
+        {
+            return 1;
+        }
+        else if(d==1)
+        {
+            return c;
+        }
+        else if(c>0 && d%2==1)
+        {
+            double low = 0;
+            double high = c;
+            double mid = (double) c / 2;
+            double ep = 0.01;
+            while (Math.abs(Math.pow(mid, d) - c) >= ep)
+            {
+                if (Math.pow(mid, d) < c) {
+                    low = mid;
+                } else {
+                    high = mid;
+                }
+                mid = (double) (high + low) / 2;
+            }
+            return mid;
+        }
+        else if(c<0 && d%2==1)
+        {
+            double low = 0;
+            Math.abs(c);
+            double high = c;
+            double mid = (double) c / 2;
+            double ep = 0.01;
+            while (Math.abs(Math.pow(mid, d) - c) >= ep) {
+                if (Math.pow(mid, d) < c) {
+                    low = mid;
+                } else {
+                    high = mid;
+                }
+                mid = (double) (high + low) / 2;
+            }
+            return mid*-1;
+        }
+        else if(c==0)
+        {
+            return 0;
+        }
+        else if(c>0 && d%2==0)
+        {
+            double low = 0;
+            double high = c;
+            double mid = (double) c / 2;
+            double ep = 0.01;
+            while (Math.abs(Math.pow(mid, d) - c) >= ep) {
+                if (Math.pow(mid, d) < c) {
+                    low = mid;
+                } else {
+                    high = mid;
+                }
+                mid = (double) (high + low) / 2;
+            }
+            return mid;
+        }
+        return 0.01;
+    }
+}
